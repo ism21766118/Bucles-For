e63d589a8933f878c2cdff98b6fb07a8b832a4b7
ism21766118 <ism21766118@a07.informatica.escoladeltreball.org>
2018-02-13 09:08:27 +0100

    Bucle For1

---
+++ Bucle_For(1).py
@@ -0,0 +1,18 @@
+#coding: utf-8
+num1=input("Escriba un numero entero aqui:")
+num2=input("Escriba un numero entero mayor aqui:")
+
+while (num1>num2):
+	print ("¡Le he pedido un numero entero mayor que"+ +str(num1)+"!")
+	num2= int(input("introduce un numero mayor entero mayor o igual que"+str(num1)+":"))
+for i in range (num1,num2):
+	
+	if i%2== 0
+	   i=i+1
+	   print "El numero"+ +str(i)+ " es un numero par"
+	else:
+	   i=i+1
+	   print "El numero"+ +str(i)+ " es un numero impar"	   
+print " programa terminado "
+	  
+
